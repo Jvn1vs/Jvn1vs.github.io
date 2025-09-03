30dac6e54a9d1db5c01db7ce2d7761e34a2d2c8e
“ThinkBook” <1915173116@qq.com>
2025-09-03 23:42:25 +0800

    Site updated: 2025-09-03 23:42:25

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/08/29/发轫--故事的开篇/","2025/08/30/是时候搭一个图床了/"];function toRandomPost(){
+var posts=["2025/08/30/是时候搭一个图床了/","2025/08/29/发轫--故事的开篇/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
